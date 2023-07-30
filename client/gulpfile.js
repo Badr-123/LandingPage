c228c5135f00cb0a50281e41cd226334786e1e48
Badr-123 <badrannabi@gmail.com>
2023-07-30 18:38:04 +0000

    fixing gulp1

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -10,22 +10,10 @@ function cssTask() {
     .pipe(gulp.dest('dist'));
 }
 
-function watchTask() {
-  gulp.watch('./src/*.css', cssTask);
-}
-
 gulp.task('css', cssTask);
-gulp.task('watch', watchTask);
 
+gulp.task('build', gulp.series(cssTask));
 
-
-// Task to build the project (clean, minify CSS, and any other tasks you need)
-gulp.task('build', gulp.series( cssTask));
-
-// Task to watch for changes in CSS files and run the 'css' task
 gulp.task('watch', function() {
   gulp.watch('./src/*.css', cssTask);
 });
-
-// Default task that runs 'build' and 'watch' tasks in parallel
-gulp.task('default', gulp.parallel('build', 'watch'));
